1a8bfebe81cbd99deeda282915f6f67eae56835d
barath0211 <barathkumaarabirami@gmail.com>
2022-09-19 10:59:56 +0530

    Check

--- spring5webapp/src/main/java/barath/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ spring5webapp/src/main/java/barath/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package barath.springframework.spring5webapp.bootstrap;
 
 import barath.springframework.spring5webapp.domain.Author;
 import barath.springframework.spring5webapp.domain.Book;
+import barath.springframework.spring5webapp.domain.Publisher;
 import barath.springframework.spring5webapp.repositories.AuthorRepository;
 import barath.springframework.spring5webapp.repositories.BookRepository;
+import barath.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +13,28 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
 private final AuthorRepository authorRepository;
 private final BookRepository bookRepository;
+private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,  PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("Scholastic Publishing");
+        publisher.setCity("Coimbatore");
+        publisher.setState("Bk");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+
 Author barath = new Author("Barath","Kumaar");
 Book psm = new Book("Power Of Subconscious Mind","1234");
 barath.getBooks().add(psm);
